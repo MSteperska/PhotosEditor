336faf022a379e8822e600d75dc7a8877f1e2f77
Gocha <msteperska@gmail.com>
2024-09-15 23:01:00 +0200

    Upload VS files

---
+++ Projekt1-testy/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Projekt1_testy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.CropImageButton = new System.Windows.Forms.Button();
+            this.SaveImageButton = new System.Windows.Forms.Button();
+            this.ScaleButton = new System.Windows.Forms.Button();
+            this.WidthLabel = new System.Windows.Forms.Label();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.WidthInput = new System.Windows.Forms.NumericUpDown();
+            this.HeightInput = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ImageSizeLabel = new System.Windows.Forms.Label();
+            this.FlipButton = new System.Windows.Forms.Button();
+            this.MirrorImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(81, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(881, 553);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(986, 36);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(170, 43);
+            this.LoadImageButton.TabIndex = 2;
+            this.LoadImageButton.Text = "Load";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CropImageButton
+            // 
+            this.CropImageButton.Location = new System.Drawing.Point(986, 97);
+            this.CropImageButton.Name = "CropImageButton";
+            this.CropImageButton.Size = new System.Drawing.Size(170, 43);
+            this.CropImageButton.TabIndex = 4;
+            this.CropImageButton.Text = "Crop";
+            this.CropImageButton.UseVisualStyleBackColor = true;
+            this.CropImageButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SaveImageButton
+            // 
+            this.SaveImageButton.Location = new System.Drawing.Point(986, 406);
+            this.SaveImageButton.Name = "SaveImageButton";
+            this.SaveImageButton.Size = new System.Drawing.Size(170, 43);
+            this.SaveImageButton.TabIndex = 5;
+            this.SaveImageButton.Text = "Save";
+            this.SaveImageButton.UseVisualStyleBackColor = true;
+            this.SaveImageButton.Click += new System.EventHandler(this.SaveImageButton_Click);
+            // 
+            // ScaleButton
+            // 
+            this.ScaleButton.Location = new System.Drawing.Point(986, 346);
+            this.ScaleButton.Name = "ScaleButton";
+            this.ScaleButton.Size = new System.Drawing.Size(170, 43);
+            this.ScaleButton.TabIndex = 8;
+            this.ScaleButton.Text = "Scale";
+            this.ScaleButton.UseVisualStyleBackColor = true;
+            this.ScaleButton.Click += new System.EventHandler(this.ScaleButton_Click);
+            // 
+            // WidthLabel
+            // 
+            this.WidthLabel.AutoSize = true;
+            this.WidthLabel.Location = new System.Drawing.Point(982, 316);
+            this.WidthLabel.Name = "WidthLabel";
+            this.WidthLabel.Size = new System.Drawing.Size(85, 20);
+            this.WidthLabel.TabIndex = 10;
+            this.WidthLabel.Text = "Width ratio";
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Location = new System.Drawing.Point(1073, 316);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(91, 20);
+            this.HeightLabel.TabIndex = 11;
+            this.HeightLabel.Text = "Height ratio";
+            // 
+            // WidthInput
+            // 
+            this.WidthInput.DecimalPlaces = 1;
+            this.WidthInput.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.WidthInput.Location = new System.Drawing.Point(986, 287);
+            this.WidthInput.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.WidthInput.Name = "WidthInput";
+            this.WidthInput.Size = new System.Drawing.Size(77, 26);
+            this.WidthInput.TabIndex = 12;
+            this.WidthInput.ValueChanged += new System.EventHandler(this.WidthInput_ValueChanged);
+            // 
+            // HeightInput
+            // 
+            this.HeightInput.DecimalPlaces = 1;
+            this.HeightInput.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.HeightInput.Location = new System.Drawing.Point(1079, 287);
+            this.HeightInput.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.HeightInput.Name = "HeightInput";
+            this.HeightInput.Size = new System.Drawing.Size(77, 26);
+            this.HeightInput.TabIndex = 13;
+            this.HeightInput.ValueChanged += new System.EventHandler(this.HeightInput_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(982, 242);
+            this.label1.MaximumSize = new System.Drawing.Size(100, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 14;
+            // 
+            // ImageSizeLabel
+            // 
+            this.ImageSizeLabel.AutoSize = true;
+            this.ImageSizeLabel.Location = new System.Drawing.Point(81, 599);
+            this.ImageSizeLabel.Name = "ImageSizeLabel";
+            this.ImageSizeLabel.Size = new System.Drawing.Size(85, 20);
+            this.ImageSizeLabel.TabIndex = 15;
+            this.ImageSizeLabel.Text = "ImageSize";
+            // 
+            // FlipButton
+            // 
+            this.FlipButton.Location = new System.Drawing.Point(986, 161);
+            this.FlipButton.Name = "FlipButton";
+            this.FlipButton.Size = new System.Drawing.Size(170, 43);
+            this.FlipButton.TabIndex = 16;
+            this.FlipButton.Text = "Flip 90 degrees right";
+            this.FlipButton.UseVisualStyleBackColor = true;
+            this.FlipButton.Click += new System.EventHandler(this.FlipButton_Click);
+            // 
+            // MirrorImageButton
+            // 
+            this.MirrorImageButton.Location = new System.Drawing.Point(986, 219);
+            this.MirrorImageButton.Name = "MirrorImageButton";
+            this.MirrorImageButton.Size = new System.Drawing.Size(170, 43);
+            this.MirrorImageButton.TabIndex = 17;
+            this.MirrorImageButton.Text = "Mirror Image";
+            this.MirrorImageButton.UseVisualStyleBackColor = true;
+            this.MirrorImageButton.Click += new System.EventHandler(this.MirrorImageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 648);
+            this.Controls.Add(this.MirrorImageButton);
+            this.Controls.Add(this.FlipButton);
+            this.Controls.Add(this.ImageSizeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HeightInput);
+            this.Controls.Add(this.WidthInput);
+            this.Controls.Add(this.HeightLabel);
+            this.Controls.Add(this.WidthLabel);
+            this.Controls.Add(this.ScaleButton);
+            this.Controls.Add(this.SaveImageButton);
+            this.Controls.Add(this.CropImageButton);
+            this.Controls.Add(this.LoadImageButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ImageEdit";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.Button CropImageButton;
+        private System.Windows.Forms.Button SaveImageButton;
+        private System.Windows.Forms.Button ScaleButton;
+        private System.Windows.Forms.Label WidthLabel;
+        private System.Windows.Forms.Label HeightLabel;
+        private System.Windows.Forms.NumericUpDown WidthInput;
+        private System.Windows.Forms.NumericUpDown HeightInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ImageSizeLabel;
+        private System.Windows.Forms.Button FlipButton;
+        private System.Windows.Forms.Button MirrorImageButton;
+    }
+}
+
---
+++ Projekt1-testy/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt1_testy
+{
+    public partial class Form1 : Form
+    {
+        //  variables for image cropping
+        private Rectangle cropRectangle;
+        private Point startPoint;
+        private Point endPoint;
+        private bool isDragging = false;
+        private Image originalImage;
+
+        //  variables for image scaling
+        private float W_ratio;
+        private float H_ratio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // loading image
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Obrazy (*.jpg;*.jpeg;*.png;*.bmp;*.jfif)|*.jpg;*.jpeg;*.png;*.bmp;*.jfif";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog () == DialogResult.OK)
+            {
+                try
+                {
+                    // Pobranie ścieżki do wybranego pliku
+                    string selectedFilePath = ofd.FileName;
+
+                    // Zwolnienie poprzedniego obrazka, jeśli istnieje
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Image.Dispose();
+                        pictureBox1.Image = null;
+                    }
+
+                    // Wczytanie nowego obrazka i wyświetlenie go w PictureBox
+                    originalImage = Image.FromFile(selectedFilePath);
+                    pictureBox1.Image = new Bitmap(originalImage);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                    //  wyświetlenie rozmiarów obrazka
+                    update_ImageSizeLabel(pictureBox1.Image.Width, pictureBox1.Image.Height);
+
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    MessageBox.Show("Błąd: Nie można załadować obrazka. Pamięć jest niewystarczająca.", "Błąd pamięci", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd: Nie można załadować obrazka. " + ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)  //crop image
+        {
+            pictureBox1.Enabled = true;
+
+            if (pictureBox1.Image == null || cropRectangle.Width == 0 || cropRectangle.Height == 0)
+                return;
+
+
+            if(pictureBox1.Image != null && cropRectangle != Rectangle.Empty)
+            {
+                Bitmap originalImage = new Bitmap(pictureBox1.Image);
+                Bitmap croppedImage = originalImage.Clone(cropRectangle, originalImage.PixelFormat);
+
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = croppedImage;              
+
+                cropRectangle = Rectangle.Empty;
+                pictureBox1.Invalidate();
+
+            }
+           
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(pictureBox1 == null)
+                return;
+
+            isDragging = true;
+            startPoint = e.Location;
+            cropRectangle = new Rectangle(startPoint, new Size(0, 0));
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDragging)
+                return;
+
+            endPoint = e.Location;
+            cropRectangle = new Rectangle(Math.Min(startPoint.X, endPoint.X),
+                                     Math.Min(startPoint.Y, endPoint.Y),
+                                     Math.Abs(startPoint.X - endPoint.X),
+                                     Math.Abs(startPoint.Y - endPoint.Y));
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+
+            if (pictureBox1.Image == null || cropRectangle.Width == 0 || cropRectangle.Height == 0)
+                return;
+
+            // Przekształcenie cropRect do współrzędnych obrazu
+            Rectangle imageRect = GetImageRectangleInPictureBox();
+            cropRectangle = TransformRectangleToImageCoordinates(cropRectangle, imageRect, pictureBox1.Image.Size);
+
+            // Przycięcie obrazka
+            Bitmap originalBitmap = new Bitmap(pictureBox1.Image);
+            Bitmap croppedImage = originalBitmap.Clone(cropRectangle, originalBitmap.PixelFormat);
+
+            originalImage.Dispose();
+            originalImage = croppedImage;
+
+            pictureBox1.Image.Dispose();
+            pictureBox1.Image = croppedImage;
+            update_ImageSizeLabel(cropRectangle.Width, cropRectangle.Height);
+
+            cropRectangle = Rectangle.Empty;
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (cropRectangle != Rectangle.Empty)
+            {
+                using (Pen pen = new Pen(Color.Blue, 1))
+                {
+                    e.Graphics.DrawRectangle(pen, cropRectangle);
+                }
+            }
+        }
+        private Rectangle AdjustRectangleToBounds(Rectangle rect, Bitmap image)
+        {
+            int x = Math.Max(rect.X, 0);
+            int y = Math.Max(rect.Y, 0);
+            int width = Math.Min(rect.Width, image.Width - x);
+            int height = Math.Min(rect.Height, image.Height - y);
+
+            return new Rectangle(x, y, width, height);
+        }
+
+
+        private Rectangle GetImageRectangleInPictureBox()
+        {
+            if (pictureBox1.Image == null)
+                return Rectangle.Empty;
+
+            float imageAspectRatio = (float)pictureBox1.Image.Width / pictureBox1.Image.Height;
+            float controlAspectRatio = (float)pictureBox1.Width / pictureBox1.Height;
+
+            int imageWidth, imageHeight;
+
+            if (imageAspectRatio > controlAspectRatio)
+            {
+                imageWidth = pictureBox1.Width;
+                imageHeight = (int)(pictureBox1.Width / imageAspectRatio);
+            }
+            else
+            {
+                imageWidth = (int)(pictureBox1.Height * imageAspectRatio);
+                imageHeight = pictureBox1.Height;
+            }
+
+            int x = (pictureBox1.Width - imageWidth) / 2;
+            int y = (pictureBox1.Height - imageHeight) / 2;
+
+            return new Rectangle(x, y, imageWidth, imageHeight);
+        }
+
+
+        private Rectangle TransformRectangleToImageCoordinates(Rectangle rect, Rectangle imageRect, Size imageSize)
+        {
+            // Przekształcenie współrzędnych z PictureBox do współrzędnych obrazu
+            float xRatio = (float)imageSize.Width / imageRect.Width;
+            float yRatio = (float)imageSize.Height / imageRect.Height;
+
+            int x = (int)((rect.X - imageRect.X) * xRatio);
+            int y = (int)((rect.Y - imageRect.Y) * yRatio);
+            int width = (int)(rect.Width * xRatio);
+            int height = (int)(rect.Height * yRatio);
+
+            return new Rectangle(x, y, width, height);
+        }
+
+        private void SaveImageButton_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image == null)
+                return;
+
+            if (pictureBox1.Image != null)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    // Ustawienie właściwości dialogu zapisu pliku
+                    saveFileDialog.Filter = "Obrazy (*.jpg;*.jpeg;*.png;*.bmp;*.jfif)|*.jpg;*.jpeg;*.png;*.bmp;*.jfif";
+                    saveFileDialog.Title = "Wybierz lokalizację zapisu";
+                    saveFileDialog.FileName = "saved_image.png"; // Domyślna nazwa pliku
+
+                    // Jeśli użytkownik wybrał lokalizację i kliknął "OK"
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Utwórz obiekt Bitmap na podstawie obrazu z PictureBox
+                        Bitmap imageToSave = new Bitmap(pictureBox1.Image);
+
+                        // Zapisz obraz do wybranej lokalizacji z dialogu
+                        imageToSave.Save(saveFileDialog.FileName);
+
+                        MessageBox.Show($"Obraz został zapisany jako {Path.GetFileName(saveFileDialog.FileName)}", "Zapisano obraz", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        // SCALE IMAGE
+
+        private void WidthInput_ValueChanged(object sender, EventArgs e)
+        {
+            W_ratio = (float)WidthInput.Value;
+        }
+
+        private void HeightInput_ValueChanged(object sender, EventArgs e)
+        {
+            H_ratio = (float)HeightInput.Value;
+        }
+
+        private void ScaleButton_Click(object sender, EventArgs e)
+        {
+            //skalowanie obrazu
+            if ((W_ratio == 0 && H_ratio == 0) || pictureBox1 == null)
+                return;
+            else
+            {
+                // new image size
+                int new_w = (int)(pictureBox1.Image.Width * W_ratio);
+                int new_h = (int)(pictureBox1.Image.Height * H_ratio);
+
+                Bitmap originalImage = new Bitmap(pictureBox1.Image);
+                //originalImage.Height = H;
+                Bitmap scaledBitmap = new Bitmap(new_w, new_h);
+
+                // Utworzenie obiektu Graphics na podstawie nowej bitmapy
+                using (Graphics g = Graphics.FromImage(scaledBitmap))
+                {
+                    // Ustawienie jakości skalowania
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                    // Skalowanie obrazu i rysowanie na nowej bitmapie
+                    g.DrawImage(originalImage, 0, 0, new_w, new_h);
+                }
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = scaledBitmap;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                update_ImageSizeLabel(new_w, new_h);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void FlipButton_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Refresh();
+
+                update_ImageSizeLabel(pictureBox1.Image.Width, pictureBox1.Image.Height);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Enabled = false;    // do wylaczenia przycinania
+        }
+
+        private void MirrorImageButton_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox1.Refresh();
+                update_ImageSizeLabel(pictureBox1.Image.Width, pictureBox1.Image.Height);
+            }
+        }
+
+        private void update_ImageSizeLabel(int w, int h)
+        {
+            ImageSizeLabel.Text = w + " x" + h;
+        }
+    }
+}
+
